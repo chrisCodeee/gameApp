45a05cbeca0568408f982d1a5232abecc87c0dd6
Chris <nzeanoruec@gmail.com>
2024-01-11 03:26:17 -0500

    Testing

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -6,7 +6,7 @@ const GameGrid = () => {
 	const { games, error } = useGames("/games");
 	return (
 		<>
-			{error && <p className="text-danger">Network Error</p>}
+			{error && <p className="text-danger">Network Error!</p>}
 			<div className="row gy-5">
 				{games.map((game) => (
 					<GameContainer className="col-12 col-sm-6 col-md-4 col-lg-3 px-3 ps-sm-3">
